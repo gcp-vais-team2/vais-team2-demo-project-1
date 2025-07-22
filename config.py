49bde297565525c4de5517a4e434bf58366633e6
avinashjay1 <Avinash.Veershetty@bristlecone.com>
2025-07-22 18:26:14 +0530

    Update content of config.py

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-config = {'db': 'https://www.smith.info/app/blog/categoryregister.php', 'api_key': '6b75d6ce30c07d59e180538461d499bdc7d66617'}
+CONFIG = {'project_id': 1, 'debug': True, 'owner': 'vais-team1'}
